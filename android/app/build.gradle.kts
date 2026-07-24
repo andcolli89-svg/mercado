aa394056c4e1368cbd7925161729acab7d0712f8
Fabio Colli <SEU_EMAIL_DO_GITHUB>
2026-07-24 20:44:43 -0300

    Migra CbOfertas V6 para Kotlin integrado do AGP 9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -35,9 +34,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 
     buildFeatures {
         compose = true
