8bf9e750603a0872cf5efe138327796ddb690ba0
Fabio Colli <SEU_EMAIL_DO_GITHUB>
2026-07-25 11:43:20 -0300

    Corrige versionName Android da CbOfertas V6 Alpha 4.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 600005
-        versionName = "6.0.0-alpha.4.1.1"
+        versionName = "6.0.0-alpha.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
